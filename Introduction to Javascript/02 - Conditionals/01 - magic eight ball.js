b543ffb9c79c406965451a3bc59325d2085f8771
Michele <43318050+Michele-91@users.noreply.github.com>
2018-12-13 15:13:49 +0100

    Update 01 - magic eight ball.js

--- Introduction to Javascript/02 - Conditionals/01 - magic eight ball.js
+++ Introduction to Javascript/02 - Conditionals/01 - magic eight ball.js
@@ -1,1 +1,30 @@
 
+
+let userName = "Michele";
+userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+let userQuestion = "Will I be able to find a coding job?";
+console.log(`The question is: ${userQuestion}`);
+const randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+switch(randomNumber) {
+  case 0: 
+    eightball = 'It is certain'; break;
+  case 1:
+    eightball = 'It is decidedly so'; break;
+  case 2:
+    eightball = 'Reply hazy try again'; break;
+  case 3:
+    eightball = 'Cannot predict now'; break;
+  case 4:
+    eightball = 'Do not count on it'; break;
+  case 5:
+    eightball = 'My sources say no'; break;
+  case 6:
+    eightball = 'Outlook not so good'; break;
+  case 7:
+    eightball = 'Signs point to yes'; break;
+  default:
+    eightball = 'The universe wants to keep this secret from you. Sorry!'; break;
+}
+
+console.log(eightball);
