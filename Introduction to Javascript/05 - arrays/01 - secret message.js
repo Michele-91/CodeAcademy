8a79703ba85bcb421277f0517e63c01552b844ba
Michele <43318050+Michele-91@users.noreply.github.com>
2018-12-13 21:47:46 +0100

    Update 01 - secret message.js

--- Introduction to Javascript/05 - arrays/01 - secret message.js
+++ Introduction to Javascript/05 - arrays/01 - secret message.js
@@ -1,1 +1,25 @@
 
+/*Using array methods, you will transform an array of strings into a secret message!*/
+
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 
+                     'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length);
+//remove last string in the array
+secretMessage.pop();
+console.log(secretMessage.length);
+//add to and program to end of array
+secretMessage.push('to', 'program');
+//change word easily in array with right
+secretMessage[7] = 'right';
+//remove first string in array
+secretMessage.shift();
+//add programming to beginning of the array
+secretMessage.unshift('Programming');
+//replace get, right, the, first, time with know
+secretMessage.splice(6, 10, 'know');
+//print secretMessage as a sentence
+console.log(secretMessage.join(' '));
+
+//console.log(secretMessage);
