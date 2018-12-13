30d23492c0849ef9bd9a9f21486fa1a637567252
Michele <43318050+Michele-91@users.noreply.github.com>
2018-12-13 14:42:36 +0100

    Update kelvin weather.js

--- Introduction to Javascript/01 - Introduction/kelvin weather.js
+++ Introduction to Javascript/01 - Introduction/kelvin weather.js
@@ -1,1 +1,14 @@
 
+//Kelvin degrees
+const kelvin = 293;
+//converts Kelvin degrees into celsius degrees
+const celsius = kelvin - 273;
+//converts celsius into fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+//math.floor rounds fahrenheit to an integer number
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
