c47f5715a7fbf1aa171e87cd5cede6e289293db1
Michele <43318050+Michele-91@users.noreply.github.com>
2018-12-15 19:34:04 +0100

    Update 01 - build a library.js

--- Introduction to Javascript/09 - Classes/01 - build a library.js
+++ Introduction to Javascript/09 - Classes/01 - build a library.js
@@ -1,1 +1,90 @@
 
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+  
+  get title() {
+    return this._title;
+  }
+  
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  
+  get ratings() {
+    return this._ratings;
+  }
+  
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
+  
+  toggleCheckOutStatus() {
+    this.isCheckedOut = !this.isCheckedOut;
+  }
+  
+  getAverageRating() {
+    let ratingSum = this.ratings.reduce((accumulator, rating) => accumulator + rating, 0);
+    return (ratingSum / this.ratings.length).toFixed(2);
+  }
+  
+  addRating(rate) {
+    this.ratings.push(rate);
+  }
+}
+
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._isCheckedOut = false;
+    this._ratings = [];
+    this._author = author;
+    this._pages = pages;
+  }
+  
+  get author() {
+    return this._author;
+  }
+  
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(title, director, runTime) {
+    super(title);
+    this._isCheckedOut = false;
+    this._ratings = [];
+    this._director = director;
+    this._runTime = runTime;
+  }
+  
+  get director() {
+    return this._director;
+  }
+  
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Speed', 'Jan de Bont', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
+
